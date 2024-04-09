413e10b00fda1583532436f67f4790bb09f1e3a5
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2024-04-09 12:35:14 -0600

    Add files via upload

--- Lectura&AnalisisDeDatos/Instrucciones.java
+++ Lectura&AnalisisDeDatos/Instrucciones.java
@@ -1,24 +1,28 @@
-import java.util.List;
+import java.io.DataOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Instrucciones {
-
 	 public void exe(int inicio) {
-		 String filePath, data;
-		 String[] trueData;
+		 File archivo, nuevo;
+		 String filePath, data, fileName;
+		 String[] trueData, title, trueTitle;
 		 int[] array = new int[3000];
 		 
-		 
 		 filePath = "D:\\Users\\182238\\Downloads";
+		 fileName = "182238.txt";
+		 nuevo = new File(fileName);
+		 FileOutputStream outFileStream;
+		
 		 for(int c = 0; c < 3000; c++) {
 			 array[c] = 0;
 		 }
 		 try {
-			 File archivo = new File(filePath, "ACT_002_emails.csv");
+			 archivo = new File(filePath, "ACT_002_emails.csv");
 			 Scanner lector = new Scanner(archivo);
 			 
 			 for(int a = 0; a < 9999; a++) {
@@ -26,7 +30,6 @@ public class Instrucciones {
 					 
 					 data = lector.nextLine();
 					 if(a >= inicio && a < (inicio+50)) {
-						 
 						 if(a > 999) {
 							 data = data.substring(11);
 						 }else if(a > 99) {
@@ -43,17 +46,52 @@ public class Instrucciones {
 							 array[b] += Integer.parseInt(trueData[b]); 
 						 }
 					 }
+					 if(a == 0) {
+						 data = data.substring(10);
+						 title = data.split(",");
+						 System.out.print(title);
+					 }
 				 }
 			 }
 			 for(int d = 0; d < 3000; d++) {
 				 System.out.print(array[d] + " ");
 			 }
-			 
 			 lector.close();
 		 }catch (FileNotFoundException e) {
 			 System.out.println("An error occurred.");
 			 e.printStackTrace();
 		 }
+		 
+		 if(nuevo.exists()) {
+			 
+		 }else {
+			 try {
+				 nuevo.createNewFile();
+			 }catch(IOException e){
+				 e.printStackTrace();
+			 }
+		 }
+		 try {
+			outFileStream = new FileOutputStream(nuevo);
+			DataOutputStream outDataStream = new DataOutputStream(outFileStream);
+			ObjectOutputStream outObjectStream;
+			try {
+				outObjectStream = new ObjectOutputStream(outDataStream);
+				
+				outObjectStream.writeObject(title[0]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				outDataStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		 
 	 }
 }
